fecc5c901b2d9e22489246aa135d125d724aa6ea
amneiht_ltu <dccntd@gmail.com>
2020-08-28 10:24:34 +0700

    Update comman.sh
    update command

--- Server/resourceSql/comman.sh
+++ Server/resourceSql/comman.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.7 ---max-allowed-packet=16777216
+sudo docker run --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.7 --max-allowed-packet=16777216
+sudo docker run --name myadmin -d --link mysql:db -p 8080:80 phpmyadmin
